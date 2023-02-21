5bffa6700ca80616b72dbfb6d539f976d7b34f0c
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-02-21 15:10:02 +0100

    red : Should start at position (4,0) and be at position (1,0) whith orientation South given commands [A, A, A, D, D]

--- src/marsRover.test.ts
+++ src/marsRover.test.ts
@@ -1,4 +1,12 @@
-import { Calc } from "./MarsRover";
-describe("Calc", () => {
-  test(")", () => {});
+import { MarsRover } from "./MarsRover";
+
+describe("Mars Rover", () => {
+  test("Should start at position (4,0) and be at position (1,0) whith orientation South given commands [A, A, A, D, D]", () => {
+    const rover = new MarsRover({ x: 0, y: 0 }, "NORTH");
+
+    rover.executeCommands(["FORWARD", "FORWARD", "FORWARD", "RIGHT", "RIGHT"]);
+
+    expect(rover.position).toStrictEqual({ x: 1, y: 0 });
+    expect(rover.orientation).toBe("NORTH");
+  });
 });
