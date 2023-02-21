67f2fa104a5d710acf69f155aae09374eae62f47
gsainteluce <gabriel.sainteluce@shodo-studio.io>
2023-02-21 15:42:43 +0100

    green : Should start (0,4) EAST and be (0,4) SOUTH given [RIGHT]

--- src/marsRover.test.ts
+++ src/marsRover.test.ts
@@ -1,7 +1,7 @@
 import { MarsRover } from "./MarsRover";
 
 describe("Mars Rover", () => {
-  test("Should start at position (4,0) and be at position (1,0) whith orientation NORTH given commands [A, A, A, D, D]", () => {
+  test("Given a rover at (4,0) NORTH when it executes commands [FORWARD, FORWARD, FORWARD, RIGHT, RIGHT] then it should be (1,0) NORTH", () => {
     const rover = new MarsRover({ x: 0, y: 4 }, "NORTH");
 
     rover.executeCommands(["FORWARD", "FORWARD", "RIGHT", "RIGHT"]);
@@ -10,28 +10,30 @@ describe("Mars Rover", () => {
     expect(rover.orientation).toBe("NORTH");
   });
 
-  test("Should start at (4,0) NORTH and be (4,0) NORTH", () => {
-    const rover = new MarsRover({ x: 0, y: 4 }, "NORTH");
+  describe("Mars rover exploration north without obstacle", () => {
+    test("Should start at (4,0) NORTH and be (4,0) NORTH", () => {
+      const rover = new MarsRover({ x: 0, y: 4 }, "NORTH");
 
-    expect(rover.position).toStrictEqual({ x: 0, y: 4 });
-    expect(rover.orientation).toBe("NORTH");
-  });
+      expect(rover.position).toStrictEqual({ x: 0, y: 4 });
+      expect(rover.orientation).toBe("NORTH");
+    });
 
-  test("Should start (0,4) NORTH and be (0,4) EAST given [RIGHT]", () => {
-    const rover = new MarsRover({ x: 0, y: 4 }, "NORTH");
+    test("Should start (0,4) NORTH and be (0,4) EAST given [RIGHT]", () => {
+      const rover = new MarsRover({ x: 0, y: 4 }, "NORTH");
 
-    rover.executeCommands(["RIGHT"]);
+      rover.executeCommands(["RIGHT"]);
 
-    expect(rover.position).toStrictEqual({ x: 0, y: 4 });
-    expect(rover.orientation).toBe("EAST");
-  });
+      expect(rover.position).toStrictEqual({ x: 0, y: 4 });
+      expect(rover.orientation).toBe("EAST");
+    });
 
-  test("Should start (0,4) EAST and be (0,4) SOUTH given [RIGHT]", () => {
-    const rover = new MarsRover({ x: 0, y: 4 }, "EAST");
+    test("Should start (0,4) EAST and be (0,4) SOUTH given [RIGHT]", () => {
+      const rover = new MarsRover({ x: 0, y: 4 }, "EAST");
 
-    rover.executeCommands(["RIGHT"]);
+      rover.executeCommands(["RIGHT"]);
 
-    expect(rover.position).toStrictEqual({ x: 0, y: 4 });
-    expect(rover.orientation).toBe("SOUTH");
+      expect(rover.position).toStrictEqual({ x: 0, y: 4 });
+      expect(rover.orientation).toBe("SOUTH");
+    });
   });
 });
